af7c346f46b6f2cf5e04d170feffa33d2ec92a5f
Oleg Komarov <o.komarov11@imperial.ac.uk>
2014-11-21 20:33:38 +0000

    [Change] rewrote plotBoundaries
    Signed-off-by: Oleg Komarov <o.komarov11@imperial.ac.uk>

--- plotBoundaries.m
+++ plotBoundaries.m
@@ -1,11 +1,10 @@
-function varargout = plotBoundaries(filename, varargin)
+function varargout = plotBoundaries(Bnd, varargin)
 
 % Parse optional inputs with defaults
 persistent ip
 if isempty(ip)
     ip = inputParser;
     ip.FunctionName = mfilename;
-    ip.addOptional('bbox'  ,[4299.36,921.69; 6289.99,2806.05],@(x) isnumeric(x) && isequal(size(x), [2,2]));
     ip.addOptional('center',[5296.12,1806.25]                ,@(x) isnumeric(x) && numel(x)== 2);
     ip.addOptional('radius',1000                             ,@(x) isnumeric(x) && isscalar(x));
     ip.addOptional('theta' ,(0:0.01:2*pi)'                   ,@(x) isnumeric(x) && isvector(x));
@@ -13,7 +12,6 @@ if isempty(ip)
     ip.addOptional('cdata' ,[]                               ,@isnumeric);
 end
 parse(ip,varargin{:})
-bbox   = ip.Results.bbox;
 center = ip.Results.center;
 radius = ip.Results.radius;
 theta  = ip.Results.theta;
@@ -26,70 +24,79 @@ else
 end
 
 % Shape structure supplied
-if isstruct(filename) && isfield(filename,'Geometry')
-    Bnd = filename;
-    
-% Import file    
-else
-    path2proj = fileparts(filename);
-    if isempty(path2proj)
-        % Assume we are in the projects folder
-        filename = fullfile('data','bdline_essh_gb','Data', filename);
-    end
-    % info = shapeinfo(filename);
-    Bnd = shaperead(filename,'boundingbox',bbox*100);
+if ~istable(Bnd)
+    error('plotBoundaries:invalidBnd','Invalid BND. Import boundaries first.')
 end
 
 % Partial if polygon intersects circle
-nBnd  = numel(Bnd);
+nBnd  = size(Bnd,1);
 ipart = false(nBnd,1);
 pop   = zeros(nBnd,2);
+bndx  = Bnd.X;
+bndy  = Bnd.Y;
 for ii = 1:nBnd
-    Bnd(ii).X = Bnd(ii).X/100;
-    Bnd(ii).Y = Bnd(ii).Y/100;
-    ipart(ii) = any(inpolygon(circ(:,1),circ(:,2), Bnd(ii).X, Bnd(ii).Y));
-    pop(ii,:) = [Bnd(ii).X(1), Bnd(ii).Y(1)];
+    bndx{ii}  = bndx{ii}/100;
+    bndy{ii}  = bndy{ii}/100;
+    ipart(ii) = any(inpolygon(circ(:,1),circ(:,2), bndx{ii}, bndy{ii}));
+    pop(ii,:) = [bndx{ii}(1), bndy{ii}(1)];
 end
-Bndpart = Bnd(ipart);
 
-% Empty if any point of the boundary not in circle
+% Separate from partial and empty (if a point of the boundary not in circle)
 iempty = ~inpolygon(pop(:,1),pop(:,2), circ(:,1),circ(:,2));
-
-% Drop
-idrop      = iempty | ipart;
-Bnd(idrop) = [];
+idrop  = iempty | ipart;
 
 % Plot
-h = NaN(2,1);
-if ~isempty(Bnd)
-    h(1) = mapshow(Bnd,'FaceColor','None','EdgeColor',[0.9880, 0.8066, 0.1794],'LineWidth',1);
-end
-if ~isempty(Bndpart)
-    h(2) = mapshow(Bndpart,'FaceColor','None','EdgeColor','k','LineWidth',1);
-end
-
-if ~isempty(cdata)
-    cdata  = cdata(~idrop);
-    isdata = cdata ~= 0;
-    minval = min(cdata(isdata));
-    cmap   = parula;
-    [a,~,bin] = histcounts(cdata, linspace(minval, max(cdata),size(cmap,1)+1));
-    ph = get(h(1),'Children');
-    npatches = numel(ph);
-    cdata = zeros(npatches,3);
-    cdata(isdata,:) = cmap(bin(isdata),:);
-    for ii = 1:npatches
-        if isdata(ii)
-            set(ph(ii),'FaceAlpha',0.5,'FaceColor',cdata(ii,:));
-        end
-    end
-   
-end
+h.filled  = plotHelper(bndx,bndy,cdata,~idrop);
+h.partial = plotHelper(bndx,bndy,[]   , ipart);
 
 if nargout == 1
     varargout{1} = h; 
 end
-if nargout == 2
-    varargout{1} = struct('Bnd',Bnd,'Bndpart',Bndpart,'Center',center,'Radius',radius,'Circle',circ); 
+end
+
+function h = plotHelper(bndx,bndy, cdata, idx)
+
+% number of patches
+bndx = bndx(idx);
+bndy = bndy(idx);
+npatches = nnz(idx);
+if npatches < 1
+    h = NaN;
+    return
+end
+
+% CData
+if ~isempty(cdata)
+    cdata           = cdata(idx);
+    isdata          = cdata ~= 0;
+    cmap            = flipud(jet(15));
+    edges           = prctile(cdata(isdata), [0,1,5,10:10:90, 95,99,100]);
+    %     minval = min(cdata(isdata));
+    %     edges = linspace(minval, max(cdata),size(cmap,1)+1);
+    [counts,a,bin]  = histcounts(cdata(isdata), edges);
+    cdata           = zeros(npatches,3);
+    cdata(isdata,:) = cmap(bin,:);
+end
+
+% Plot contour line
+x = cell2mat(bndx');
+y = cell2mat(bndy');
+h = line(x,y,'LineWidth',1,'Visible','off');
+if isempty(cdata)
+    set(h,'Visible','on')
+else
+    h(2) = hggroup('Tag','colored','Visible','off');
+    for ii = 1:npatches
+        if isdata(ii)
+            x = bndx{ii}(1:end-1);
+            y = bndy{ii}(1:end-1);
+            patch(x,y,ones(1,numel(x)),'Parent',h(2),...
+                  'FaceColor',cdata(ii,:),'FaceAlpha',0.5,'EdgeColor','none');
+        end
+    end
+    drawnow
+    set(h(2),'Visible','on')
 end
 end
+
+
